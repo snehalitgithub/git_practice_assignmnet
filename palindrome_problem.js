4cdca498209b646b74a6f3db301e99b9dce797a5
snehalitgithub <snehalbanthiya@gmail.com>
2022-10-20 17:33:36 +0530

    check the problem do the solution

---
+++ palindrome_problem.js
@@ -0,0 +1,11 @@
+//check whether number is palindrome or not
+
+var ispalindrome = (num);
+
+for(var a = 5;a>1;a++){
+    for(var b = 1; b<a;b++){
+        console.log("palindrome")
+    }else 
+    console.log("notpalindrome")
+}
+
---
+++ prime_problem.js
@@ -0,0 +1,6 @@
+// check whether the number is prime or not
+let num = 9;
+for(let i=0;i<num;i++){
+console.log(prime)
+}
+    }
